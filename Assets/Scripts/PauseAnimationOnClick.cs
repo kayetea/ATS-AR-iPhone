51c0197c1f4393e3157a78a7580c4c370e55febf
Katelyn <godfrey.katelyn@gmail.com>
2016-05-25 10:39:34 -0400

    initial commit

---
+++ Assets/Scripts/PauseAnimationOnClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseAnimationOnClick : MonoBehaviour {
+
+	private Animator anim; 
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	public void OnMouseDown()
+	{
+		//turn on or off the animation when clicking on 3d model
+		if (anim.enabled)
+		{
+			anim.enabled = false;
+		}
+		else{
+			anim.enabled = true; 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
