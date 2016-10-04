74a1dd8b81be9aee57dcf14b4bfc3185f8f36939
Katelyn <godfrey.katelyn@gmail.com>
2016-10-04 15:18:14 -0400

    Added ATFS product page

---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour {
+
+	public string sceneName;
+
+    //private GoogleAnalyticsV4 googleAnalytics;
+
+	// Use this for initialization
+	void Start () {
+       // googleAnalytics = GameObject.Find("GAv4").GetComponent<GoogleAnalyticsV4>();
+	}
+	
+	public void Exit() {
+		Application.Quit(); 
+	}
+
+	public void ChangeScene(){
+        SceneManager.LoadScene(sceneName);
+		Resources.UnloadUnusedAssets ();
+
+       // googleAnalytics.LogScreen(sceneName);
+	}
+
+	public void OnMouseDown(){
+        SceneManager.LoadScene(sceneName);
+        Resources.UnloadUnusedAssets ();
+      //  googleAnalytics.LogScreen(sceneName);
+    }
+}
