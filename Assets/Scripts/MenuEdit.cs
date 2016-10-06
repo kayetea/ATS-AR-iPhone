b6bb63fef0d9c6738022a6e9f7112a34bcedbe9c
Katelyn <godfrey.katelyn@gmail.com>
2016-10-06 11:48:23 -0400

    menu updates and linking

---
+++ Assets/Scripts/MenuEdit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuEdit : MonoBehaviour {
+
+	public string sceneName;
+
+	void Start() {
+		if(sceneName == SceneManager.GetActiveScene().name) 
+		{
+			DisableButton();
+		}
+	}
+
+	private void DisableButton()
+	{
+		GetComponent<Image>().color = new Color32(71, 71, 71, 255);
+		GetComponentInChildren<Text>().color = new Color32(100, 100, 100, 255);
+	}
+}
