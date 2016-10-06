97db0c6c2b86a37e881e197136555de3c44319c9
Katelyn <godfrey.katelyn@gmail.com>
2016-10-06 10:01:19 -0400

    simplified rotateobj

--- Assets/Scripts/RotateObj.cs
+++ Assets/Scripts/RotateObj.cs
@@ -1,85 +1,16 @@
-﻿
-using UnityEngine; 
-using System.Collections; 
+﻿using UnityEngine;
+using System.Collections;
 
-public class RotateObj: MonoBehaviour{ 
-	public Transform target;
-	public float distance = 5.0f; 
-	public float xSpeed = 60.0f; 
-	public float ySpeed = 60.0f; 
-	public float yMinLimit = 10f;
-	public float yMaxLimit = 60f; 
-	public float distanceMin = 5f; 
-	public float distanceMax = 10f; 
-	float x = 0.0f; 
-	float y = 0.0f; 
+public class RotateObj : MonoBehaviour 
+{
+	float rotSpeed = 20;
 
-	void Start() { 
-		Vector3 angles = transform.eulerAngles; 
-		x = angles.y; 
-		y = angles.x; 
+	void OnMouseDrag()
+	{
+		float rotX = Input.GetAxis("Mouse X")*rotSpeed*Mathf.Deg2Rad;
+		//float rotY = Input.GetAxis("Mouse Y")*rotSpeed*Mathf.Deg2Rad;
 
-		// Make the rigid body not change rotation 
-		if(GetComponent<Rigidbody>()) 
-		{
-			GetComponent<Rigidbody>().freezeRotation = true;
-		}
-	} 
-
-	void Update(){ 
-		Debug.Log("beep");
-		//Just orbit touch without movement
-		if (target && Input.touchCount == 1 && Input.GetTouch(0).position.x > Screen.width / 2 && Input.GetTouch(0).phase == TouchPhase.Moved) 
-		{ 
-			Debug.Log("Orbiting! 1 touch"); 
-			Orbit(Input.GetTouch(0)); 
-		} 
-		else if (Input.touchCount == 2) 
-		{ 
-			if(Input.GetTouch(0).position.x > Screen.width / 2 && Input.GetTouch(0).phase == TouchPhase.Moved)
-			{
-				Orbit(Input.GetTouch(0)); //Movement was touched second 
-			}
-			else if (Input.GetTouch(1).position.x > Screen.width / 2 && Input.GetTouch(1).phase == TouchPhase.Moved) 
-			{
-				Orbit(Input.GetTouch(1)); //Movement was touched first 
-			}
-		} 
-	} 
-
-	void Orbit(Touch touch) {
-		x += touch.deltaPosition.x * xSpeed * 0.02f /* * distance*/; 
-		y -= touch.deltaPosition.y * ySpeed * 0.02f /* * distance*/; 
-		y = ClampAngle(y, yMinLimit, yMaxLimit); 
-
-		Quaternion rotation = Quaternion.Euler(y, x, 0); 
-
-		//distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax); 
-
-		RaycastHit hit; 
-
-		if (Physics.Linecast(target.position, transform.position, out hit)) 
-		{ 
-			// distance -= hit.distance; 
-		} 
-
-		Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance); 
-		Vector3 position = rotation * negDistance + target.position; transform.rotation = rotation;
-		transform.position = position; 
-	} 
-
-	public static float ClampAngle(float angle, float min, float max) 
-	{ 
-		if (angle < -360F)
-		{
-			angle += 360F; 
-		}
-		if (angle > 360F) 
-		{
-			angle -= 360F; 
-		}
-
-		return Mathf.Clamp(angle, min, max); 
-	} 
-
+		transform.RotateAround(Vector3.up, -rotX);
+		//transform.RotateAround(Vector3.right, rotY);
+	}
 }
